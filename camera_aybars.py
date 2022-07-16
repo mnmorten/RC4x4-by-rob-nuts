b7fa0a6bc75b62e7f35720f64c9ab88fc63647e9
sezenoyku <54738215+sezenoyku@users.noreply.github.com>
2022-07-16 09:07:38 +0300

    Create camera_aybars.py

---
+++ camera_aybars.py
@@ -0,0 +1,134 @@
+
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+#####################################################
+##              Align Depth to Color               ##
+#####################################################
+
+# First import the library
+#from scipy.interpolate import interp1d
+import pyrealsense2 as rs
+# Import Numpy for easy array manipulation
+import numpy as np
+# Import OpenCV for easy image rendering
+import cv2
+from PIL import Image as im
+
+# Create a pipeline
+pipeline = rs.pipeline()
+
+# Create a config and configure the pipeline to stream
+#  different resolutions of color and depth streams
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+found_rgb = False
+for s in device.sensors:
+    if s.get_info(rs.camera_info.name) == 'RGB Camera':
+        found_rgb = True
+        break
+if not found_rgb:
+    print("The demo requires Depth camera with Color sensor")
+    exit(0)
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+profile = pipeline.start(config)
+
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+
+# We will be removing the background of objects more than
+#  clipping_distance_in_meters meters away
+clipping_distance_in_meters = 1 #1 meter
+clipping_distance = clipping_distance_in_meters / depth_scale
+
+steering = 320
+vel_esc = 1600
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+# Streaming loop
+try:
+    while True:
+        # Get frameset of color and depth
+        frames = pipeline.wait_for_frames()
+        # frames.get_depth_frame() is a 640x360 depth image
+
+        # Align the depth frame to color frame
+        aligned_frames = align.process(frames)
+
+        # Get aligned frames
+        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+        color_frame = aligned_frames.get_color_frame()
+
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            continue
+
+        depth_image = np.asanyarray(aligned_depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+       
+        # Remove background - Set pixels further than clipping_distance to grey
+
+        black_color = 0
+        orange_color_low = np.array([0,80,200])
+        orange_color_high = np.array([102,170,255])
+
+        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+        bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), black_color, color_image)
+        
+        bg_removed_red = np.where(bg_removed[:,:,2] >= orange_color_low[2], bg_removed[:,:,2], black_color)
+        bg_removed_green = np.where((bg_removed[:,:,1] >= orange_color_low[1]) & (bg_removed[:,:,1] <= orange_color_high[1]), bg_removed_red, black_color)
+        bg_removed_blue = np.where((bg_removed[:,:,0] >= orange_color_low[0]) & (bg_removed[:,:,0] <= orange_color_high[0]), bg_removed_green, black_color)
+               
+        count_nonblack = np.count_nonzero(bg_removed_blue)
+        if count_nonblack == 0:
+            pass
+            vel_esc = 1600
+        else:
+            non_black_mean = np.sum(np.nonzero(bg_removed_blue)[1])
+            result = non_black_mean / count_nonblack
+            
+            #normed_arr = interp1d([0,640],[130,55])
+            #normed_result = int(normed_arr(result))
+            
+            vel_esc = 1680
+        steering = int(result)
+        print(vel_esc)
+        print(steering)
+        # Render images:
+        #   depth align to color on left
+        #   depth on right
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
+        cv2.imshow('Align Example', bg_removed_blue)
+        key = cv2.waitKey(1)
+        # Press esc or 'q' to close the image window
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
+finally:
+    pipeline.stop()
+    
+    
+   
+    
